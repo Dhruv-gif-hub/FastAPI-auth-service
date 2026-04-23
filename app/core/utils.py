f047d51a8d70ebbf421cc17567714f4384b84a7f
Dhruv Bhatt <dhruvbhatt938@gmail.com>
2026-04-23 00:43:41 -0700

    Fifteenth Commit

--- app/core/utils.py
+++ app/core/utils.py
@@ -4,5 +4,6 @@ password_hash = PasswordHash.recommended()
 
 blacklisted_tokens = set()
 
+
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return password_hash.verify(plain_password, hashed_password)
