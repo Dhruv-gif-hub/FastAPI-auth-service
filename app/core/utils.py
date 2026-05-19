21ff597fb1fbb2a5db0dedc07a756e882fd7cec8
Dhruv Bhatt <dhruvbhatt938@gmail.com>
2026-05-19 02:06:15 -0700

    Forty-second Commit

--- app/core/utils.py
+++ app/core/utils.py
@@ -4,5 +4,6 @@ password_hash = PasswordHash.recommended()
 
 blacklisted_tokens = set() # Set to store the blacklisted tokens that are no longer valid
 
+# Function to hash a password
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return password_hash.verify(plain_password, hashed_password)
