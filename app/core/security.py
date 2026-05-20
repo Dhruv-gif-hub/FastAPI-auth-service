b9b491eba81f726690cc42a92e80fee1930ff20e
Dhruv Bhatt <dhruvbhatt938@gmail.com>
2026-05-20 04:10:59 -0700

    Forty-third Commit

--- app/core/security.py
+++ app/core/security.py
@@ -49,7 +49,8 @@ def create_refresh_token(data: dict, expires_delta: timedelta | None = None):
     return encoded_jwt
 
 def get_current_user(security_scopes: SecurityScopes,
- # Security is used to declare that this dependency is required for the endpoint, and it will use the oauth2_scheme to extract the token from the request.
+ # Security is used to declare that this dependency is required for the endpoint, 
+ # and it will use the oauth2_scheme to extract the token from the request.
         token: Annotated[str, Security(oauth2_scheme)],
         request: Request,
         db : Annotated[Database, Depends(get_db)]):
