12646ece2387e3bbe9a5a0c6ecdd6c57bb9646b2
Dhruv Bhatt <dhruvbhatt938@gmail.com>
2026-05-22 05:07:28 -0700

    Forty-fifth Commit

--- app/core/config.py
+++ app/core/config.py
@@ -1,16 +1,16 @@
 from pydantic import Field
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
-class Config(BaseSettings):
-    SECRET_KEY: str = Field(..., min_length=6)
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(default=60, gt=0, le=1440)
-    REFRESH_EXPIRE_DAYS: int = Field(default=5, gt=0, le=12)
-    HASH_KEY: str = Field(..., min_length=4)
+class Settings(BaseSettings):
+    SECRET_KEY: str 
+    ALGORITHM: str 
+    ACCESS_TOKEN_EXPIRE_MINUTES: int 
+    REFRESH_EXPIRE_DAYS: int 
+    HASH_KEY: str 
 
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8"
         )
 
-config = Config()
+config = Settings()
