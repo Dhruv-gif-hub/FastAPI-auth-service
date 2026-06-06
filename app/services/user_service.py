bebde247c75d445283c937a2bfaf9a4a30bb8f76
Dhruv Bhatt <dhruvbhatt938@gmail.com>
2026-06-06 12:59:23 -0700

    Sixtieth Commit

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -4,7 +4,9 @@ from ..repositories.user_repository import UserRepository
 from ..dependencies.service_dependencies import get_user_repo, get_redis
 from fastapi import Depends 
 from typing import Annotated
-
+ 
+# This act as a layer between the user and the database.
+# From here the user's requests are mapped with the relevant database query function.
 class UserService:
     
     def __init__(self, user_repo : Annotated[UserRepository, Depends(get_user_repo)], 
