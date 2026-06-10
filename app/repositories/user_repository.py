c8e24d3654b50f0663b81e2e2aa6f0dbef8ff117
Dhruv Bhatt <dhruvbhatt938@gmail.com>
2026-06-10 09:51:08 -0700

    Sixty-third Commit

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -8,6 +8,7 @@ from fastapi import HTTPException, Depends
 from ..dependencies.db import get_db
 from uuid import UUID
 
+# This covers method that are directly access databases directly.
 class UserRepository:
 
     def __init__(
