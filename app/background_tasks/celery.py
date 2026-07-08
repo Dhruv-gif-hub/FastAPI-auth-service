44e5525522c62fe02cbfc2ca47b1e62b05d9efe3
Dhruv Bhatt <dhruvbhatt938@gmail.com>
2026-07-08 05:20:29 -0700

    Seventy-seventh Commit

--- app/background_tasks/celery.py
+++ app/background_tasks/celery.py
@@ -1,4 +1,4 @@
-from celery import Celery
+from celery import Celery, shared_task
 from ..core.config import Settings
 
 broker_url = (
@@ -21,3 +21,26 @@ celery = Celery(
 
     backend=backend_url
 )
+
+celery.conf.update(
+
+    task_serializer="json",
+
+    result_serializer="json",
+
+    accept_content=["json"],
+
+    timezone="UTC",
+
+    enable_utc=True,
+
+    result_expires=3600,
+
+    broker_connection_retry_on_startup=True,
+
+    task_track_started=True,
+)
+
+@shared_task
+def update_post(id, user, blog, post):
+    return post.update_blog(id, user, blog)
